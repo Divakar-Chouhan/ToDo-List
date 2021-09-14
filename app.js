3076a65ce924b6135ead1c377edbf2eb5c7934a7
Divakar chouhan <divakarchouhan251@gmail.com>
2021-09-14 21:01:23 +0530

    fix bug in app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get("/about",function(req,res){
 app.get("/work",function(req,res){
     res.render("list",{ListTitle:"work",newListItems:workItems});
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("server is running on port 3000");
     
 })
